2e926a311fa8f46f8564f741f08e6a25b8a46b40
yifei huang <easyfly.huang@gmail.com>
2016-05-25 22:19:36 +0800

    建立 user 和userinfo 表
    包括index， varcha， foreign key 等

--- weibo.sql
+++ weibo.sql
@@ -3,6 +3,33 @@ create database db_weibo default charset utf8;
 use db_weibo;
 
 create table tb_user(
-	id unsign int not null primary key auto_increment;
-	
+	id int not null primary key auto_increment,
+	account char(20) not null,
+	password char(40) not null,
+	registime int(10) not null,
+	logintime int(20) not null,
+	locked tinyint(1) default 0
 );
+
+create table tb_userinfo(
+	id int not null primary key auto_increment,
+	uid int not null,
+	username char(45) not null,
+	truename char(45),
+	sex enum('f','m') default 'm',
+	location char(45) not null default '',
+	constellation char(10) not null default '',
+	intro varchar(100) not null default '',
+	face50 varchar(100) not null,
+	face80 varchar(100) not null,
+	face180 varchar(100) not null,
+	style varchar(45) not null default 'default',
+	follow int not null default 0,
+	fans int not null default 0,
+	weibo int not null default 0,
+	foreign key (uid) references tb_user (id)
+);
+
+
+
+
