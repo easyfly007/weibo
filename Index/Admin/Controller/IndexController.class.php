c5c6f0e1d3e81f9499fd10846dc729bb9524ad66
yifei huang <easyfly.huang@gmail.com>
2016-07-02 10:41:45 +0800

    后台管理系统的展示

--- Index/Admin/Controller/IndexController.class.php
+++ Index/Admin/Controller/IndexController.class.php
@@ -12,6 +12,15 @@ class IndexController extends CommonController {
     }
 
     public function copy(){
+        $this->usercnt = M('user')->count();
+        $this->lockedcnt = M('user')->where(array('locked'=>1,))->count();
+        // 原创
+        $where = array('original'=>0);
+        $this->orgweibocnt = M('weibo')->where($where)->count();
+        // 转发
+        $this->fwdweibocnt = M('weibo')->count() - $this->orgweibocnt; 
+        // 评论
+        $this->commentcnt = M('comment')->count();
     	$this->display();
     }
 
